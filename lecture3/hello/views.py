7ba4e53a13d912a2a49ae763d109ce42b51f2874
Suhani1608 <9835suhani@gmail.com>
2026-01-31 16:25:32 +0530

    arranging files

---
+++ lecture3/hello/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,"hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Mr. Brian")
+
+def david(request):
+    return HttpResponse("Hello, Mr. and Mrs. David")
+
+def greet(request,name):
+    return HttpResponse(f"Hello,{name.capitalize()}")
--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Analyzer project.
+URL configuration for lecture3 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include , path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("hello/",include("hello.urls"))
+    path('hello/',include("hello.urls"))
 ]
